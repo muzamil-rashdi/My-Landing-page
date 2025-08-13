ac47dfe28017954ff10e1e9f9d4202f6caffdeeb
Muhammad Muzamil <muhammadmuzamil.bscsf21@iba-suk.edu.pk>
2025-08-13 08:45:17 -0700

    added hero

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 function App() {
   return (
+    <>
         <Navbar/>
+        <Hero />
+        </>
   );
 }
 
